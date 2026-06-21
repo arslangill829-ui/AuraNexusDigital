01a59c97bac7e644e37292522ffc1b8b03b480d2
arslangill829-ui <arslangill829@gmail.com>
2026-06-21 14:01:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  base: '/AuraNexusDigital/',//
   return {
+    base: '/AuraNexusDigital/',//
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
